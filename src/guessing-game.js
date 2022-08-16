591013fe8caa971211d78b1ddb5d5ede364a1fb4
chizzora <opticalzaxar2001@gmail.com>
2022-08-16 20:27:52 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+
+	}
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	return this.res = Math.ceil(this.min + (this.max - this.min)/2);
     }
 
     lower() {
-
+    	this.setRange(this.min, this.res);
     }
 
     greater() {
-
+    	this.setRange(this.res, this.max);
     }
 }
 
